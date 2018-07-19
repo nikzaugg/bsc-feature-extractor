89c8bf056828cc75023fec36578dcfc3cc3cd362
nikzaugg <zauggnik@gmail.com>
2018-07-19 18:24:59 +0200

    test preprocess-dataset added to extract features

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
+import pandas as pd
+import numpy as np
 
 
 def main():
+    """ Main entry point to compute features """
+
 
     ########################
     # DATA CREATED BEFORE  #
@@ -25,11 +29,12 @@ def main():
         # list() tf-idf features
         # list() ck-metrics
         # list() scc-metrics
+        # list() labels
 
         # 5) fetch patch of file & fetch patch of previous patchset/base
 
         # 6) compute Code-Metrics -> save features
-        
+
         # 7) compute checkstyle-metrics -> save features
 
         # 8) compute pmd-metrics -> save features
@@ -42,10 +47,11 @@ def main():
 
         # 12) lookup ChangeDistiller types -> save features
 
-        # 13) add each feature list to features
+        # 13) extract labels --> save
 
-    
-    # 14) print each feature-row into a data-set 
+        # 14) add each feature list to features
+
+    # 15) print each feature-row into a data-set
 
     pass
 
